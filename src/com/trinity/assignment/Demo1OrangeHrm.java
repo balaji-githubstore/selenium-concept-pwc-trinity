ea104c791688615a080d0906d57b8b4a62b0585c
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-13 15:15:45 +0530

    removed space

--- src/com/trinity/assignment/Demo1OrangeHrm.java
+++ src/com/trinity/assignment/Demo1OrangeHrm.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo1OrangeHrm {
 	
 	public static void main(String[] args) {
-		
 		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "D:\\Software\\geckodriver.exe");
 	
